79b7cd2ad7422ffc19937dce3565f53ce2cc0be7
srinakorn-lab <srinakornkotana@gmail.com>
2026-05-28 20:43:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+wwindow.CCU_CONFIG = {
+  supabaseUrl: "https://yqlwxlfgwjjtomqukazs.supabase.co",
+  supabaseAnonKey: "sb_publishable_w6TdkZkc0COHD772Qsn5vw_oSpa48KO",
+  adminEmails: ["admin@ccu.local"],
+  defaultDeptByEmailPrefix: true,
+  appName: "CCU System",
+  storageKey: "ccu_v3",
+};
