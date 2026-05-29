23fdd044a181b498fa64e5c781f633beb84aa92b
srinakorn-lab <srinakornkotana@gmail.com>
2026-05-29 07:08:55 +0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,8 +1,1 @@
-window.CCU_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  adminEmails: ["admin@ccu.local"],
-  defaultDeptByEmailPrefix: true,
-  appName: "CCU System",
-  storageKey: "ccu_v3",
-};
+
